32586788459f922cf583b4735003a8216b9535c7
JosiasAG <mirror300@hotmail.com>
2023-05-08 13:00:19 -0600

    render

--- imshop/settings.py
+++ imshop/settings.py
@@ -90,10 +90,10 @@ WSGI_APPLICATION = 'imshop.wsgi.application'
 }'''
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600,
+    )
 }
 
 # Password validation
